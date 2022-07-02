54c0a025318329978f6f4ca8f2597926351f38ed
Azizbek Rahbarqulov <azizbekrakhbarkulov9174@gmail.com>
2022-07-02 16:57:05 +0500

    second commit

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'shop',
+    'blog',
+    'product',
+    'pages'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +60,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
