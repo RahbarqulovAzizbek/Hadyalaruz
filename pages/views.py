6da611c4d748af2d6616ccd63d1e769665e2362f
Azizbek Rahbarqulov <azizbekrakhbarkulov9174@gmail.com>
2022-07-12 23:34:05 +0500

    Create  model

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import reverse
+from django.views.generic import TemplateView, CreateView
+from pages.forms import ContactModelForm
 
 
 class HomePageView(TemplateView):
     template_name = 'main/index.html'
+
+class ContactView(CreateView):
+    form_class = ContactModelForm
+    template_name = 'main/contact.html'
+
+    def get_success_url(self):
+        return reverse('pages:contact')
